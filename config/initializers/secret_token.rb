d27fa57a73613d63654520594bfc0f3730d1315d
Andreas Arnold <andreas@mint.(none)>
2011-12-12 19:40:47 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmdrepository::Application.config.secret_token = '816ab8c4858aefc5a62d24e804d72dd7116d2446614cc869ff9c9bf86e702c16691b23e554d3a6aecb1be6b70ca9c600da805fc997a6b2e61fa1c83d40c360bb'
